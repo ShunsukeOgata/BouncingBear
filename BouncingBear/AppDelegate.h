d1d9e4c887de45cd0e453b199d914118070f8d43
Mighty <mighty@Shunsuke-no-MacBook-Air.local>
2014-11-04 16:32:40 +0900

    Initial Commit

---
+++ BouncingBear/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BouncingBear
+//
+//  Created by Mighty on 2014/11/04.
+//  Copyright (c) 2014年 Shunsuke Ogata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BouncingBear/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BouncingBear
+//
+//  Created by Mighty on 2014/11/04.
+//  Copyright (c) 2014年 Shunsuke Ogata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
